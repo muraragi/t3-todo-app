4bfb46b756b9509e72a11a2e10a2ddce474a93e3
muraragi <muraragi99@gmail.com>
2022-07-11 00:39:45 +0300

    removed unused example route

--- src/server/router/index.ts
+++ src/server/router/index.ts
@@ -2,13 +2,11 @@
 import { createRouter } from './context'
 import superjson from 'superjson'
 
-import { exampleRouter } from './example'
 import { authRouter } from './auth'
 import { todoRouter } from './todo'
 
 export const appRouter = createRouter()
   .transformer(superjson)
-  .merge('example.', exampleRouter)
   .merge('auth.', authRouter)
   .merge('todo.', todoRouter)
 
